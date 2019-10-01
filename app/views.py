417a4320b18ec9555ed333b8aee0fc8bfec0f767
Kostiantyn Bushko <kbushko@intellias.com>
2019-10-01 19:16:25 +0300

    flask send_file simple example
    Signed-off-by: Kostiantyn Bushko <kbushko@intellias.com>

--- app/views.py
+++ app/views.py
@@ -1,8 +1,16 @@
-#!/usr/bin/python
 from app import app
+from flask import send_file
 
 
 @app.route('/')
 def index():
     return "Flask application template! Home Page"
 
+
+@app.route('/download')
+def download():
+    filename = 'README.md'
+    response = send_file('../{}'.format(filename), attachment_filename=filename, as_attachment=True)
+    response.headers["x-filename"] = filename
+    response.headers["Access-Control-Expose-Header"] = 'x-filename'
+    return response
